2faed0ff93aa48b8e0b946701d770db3d1ade650
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-11-30 22:43:59 +0000

    Speedup containerised compilation

--- project.clj
+++ project.clj
@@ -7,10 +7,12 @@
         [com.fasterxml.jackson.core/jackson-databind "2.4.3"]
 	[junit "4.11"]
         [org.jodd/jodd "3.1.1"]
+        [org.apache.commons/commons-io "1.3.2"]
 
         ;;[org.clojure/clojure "{{clojure-version}}"]
     ]
   :java-source-paths ["src/main/java"]
   :resource-paths ["src/main/resources"]
   :profiles { :uberjar {:aot :all} }
+  :local-repo "local-m2"
   :main com.seigneurin.spark.IndexTweets)
