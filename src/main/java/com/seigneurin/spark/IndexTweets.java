29c24892b2e89b02773220a6634f4606caac6b41
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-11-30 17:30:09 +0000

    Use our URLs

--- src/main/java/com/seigneurin/spark/IndexTweets.java
+++ src/main/java/com/seigneurin/spark/IndexTweets.java
@@ -33,14 +33,14 @@ public class IndexTweets {
 
         // Spark
         SparkConf sparkConf = new SparkConf()
-                .setAppName("Tweets Android")
-                .setMaster("local[2]")
+                .setAppName("Tweets Docker")
+                .setMaster("spark://spark-master-gce.weave.local:7077")
                 .set("spark.serializer", KryoSerializer.class.getName())
-                .set("es.nodes", "localhost:9200")
+                .set("es.nodes", "elasticsearch-aws-3.weave.local:9200")
                 .set("es.index.auto.create", "true");
         JavaStreamingContext sc = new JavaStreamingContext(sparkConf, new Duration(5000));
 
-        String[] filters = { "#Android" };
+        String[] filters = { "#Docker" };
         TwitterUtils.createStream(sc, twitterAuth, filters)
                 .map(s -> new Tweet(s.getUser().getName(), s.getText(), s.getCreatedAt(), detectLanguage(s.getText())))
                 .map(t -> mapper.writeValueAsString(t))
