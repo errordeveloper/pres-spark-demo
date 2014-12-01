5cd1a5ed115d0540b0dace17ba67d4fbeaa8675b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-12-01 13:01:58 +0000

    Print Java command and set twitter4j logger

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@ git reset --hard myfork/master
 docker run -v `pwd`:/io errordeveloper/lein uberjar \
   && sudo weave run --with-dns 10.10.1.55/24 \
     --entrypoint spark-submit -h spark-submit-aws-5.weave.local \
+    -e SPARK_PRINT_LAUNCH_COMMAND=1 \
     -v ~/pres-spark-demo/:/io errordeveloper/weave-spark-shell-minimal \
     --master spark://spark-master-gce.weave.local:7077 \
     --class com.seigneurin.spark.IndexTweets \
+    --driver-java-options "-Dtwitter4j.loggerFactory=twitter4j.Log4JLoggerFactory" \
     /io/target/pres-spark-demo-0.0.1-standalone.jar \
     spark://spark-master-gce.weave.local:7077
